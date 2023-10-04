9497e39f133191bb279f29e469bcaa0483c39f45
EvstiAL <realtok05@yandex.ru>
2023-10-04 19:29:22 +0300

    04.10

--- Hello.py
+++ Hello.py
@@ -24,13 +24,25 @@ def run():
         page_icon="👋",
     )
 
-    st.write("Добро пожаловать в InfotestCODE!")
+    st.write("# Welcome to Streamlit! 👋")
 
-    st.sidebar.success("Введите логин и пароль:) Он такой же как и на обычном сайте)")
+    st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-ТЕСТ ТЕСТ ТЕСТ ТЕСТ
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+        **👈 Select a demo from the sidebar** to see some examples
+        of what Streamlit can do!
+        ### Want to learn more?
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+        ### See more complex demos
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+          Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
     """
     )
 
